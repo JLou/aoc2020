d9a53e438fb68dfda3922a483cd149b8d8abdcd7
Jean-Lou PIERME <jeanlou.pierme@axa.fr>
2020-12-11 11:52:08 +0100

    day2 part 2

--- 02.py
+++ 02.py
@@ -6,4 +6,9 @@ lines = [re.split(pattern, x)[1:-1]
          for x in open("inputs/02.txt").read().split('\n') if x]
 valid = len(list((password for min, max, char, password in lines
                   if int(min) <= password.count(char) <= int(max))))
-print(valid)
+print("Part 1:" + str(valid))
+
+# Part 2
+valid = (list((password for min, max, char, password in lines
+               if (password[int(min)-1] == char) ^ (password[int(max)-1] == char))))
+print("Part 2:" + str(len(valid)))
