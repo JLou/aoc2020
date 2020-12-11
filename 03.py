2e8c0fd68a24cc35433a7142bcf5401263f9dc55
Jean-Lou PIERME <jeanlou.pierme@axa.fr>
2020-12-11 14:38:32 +0100

    day3 part 2

--- 03.py
+++ 03.py
@@ -1,3 +1,5 @@
+from functools import reduce
+
 filename = 'inputs/03.txt'
 
 with open(filename, 'r') as f:
@@ -11,4 +13,22 @@ for line in lines:
         treeCount += 1
     x = (x+3) % lineLength
 
-print(treeCount)
+print("Part 1:" + str(treeCount))
+
+# Part 2
+
+slopes = [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)]
+
+counts = []
+for slopeX, slopeY in slopes:
+    x = 0
+    treeCount = 0
+    for y in range(0, len(lines), slopeY):
+        line = lines[y]
+        if line[x] == '#':
+            treeCount += 1
+        x = (x+slopeX) % lineLength
+    counts.append(treeCount)
+
+product = reduce((lambda x, y: x * y), counts)
+print(product)
