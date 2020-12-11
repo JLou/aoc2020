a109c03fd366f08db3bf239252f2c85b4ce87a40
Jean-Lou PIERME <jeanlou.pierme@axa.fr>
2020-12-11 10:50:58 +0100

    day 1 part 2

--- 1.py
+++ 1.py
@@ -1,13 +1,23 @@
-f = open("inputs/1.txt", "r")
+from itertools import combinations
+# f = open("inputs/1.txt", "r")
 
-numbers = set()
-for l in f:
-    x = int(l)
-    complement = 2020-x
-    if complement in numbers:
-        print("combo found:" + str(x) + " " + str(complement))
-        print('their multiplication is:' + str(x*complement))
-    else:
-        numbers.add(x)
+# numbers = set()
+# for l in f:
+#     x = int(l)
+#     complement = 2020-x
+#     if complement in numbers:
+#         print("combo found:" + str(x) + " " + str(complement))
+#         print('their multiplication is:' + str(x*complement))
+#     else:
+#         numbers.add(x)
 
-f.close()
+# f.close()
+
+# part 2
+
+
+numbers = list(map(int, filter(None, open("inputs/1.txt").read().split('\n'))))
+# perm = filter(lambda x: sum(x) == 2020, combinations(numbers, 3))[0]
+perm = (x*y*z for x, y, z in combinations(numbers, 3) if sum((x, y, z)) == 2020)
+
+print(list(perm))
