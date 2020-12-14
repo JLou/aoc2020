0603adcec532bd0bd2f50e253ecdd1ec053bb87b
Jean-Lou PIERME <jeanlou.pierme@axa.fr>
2020-12-14 10:37:08 +0100

    day 6 part 2

--- 06.py
+++ 06.py
@@ -1,3 +1,4 @@
+# Part 1
 with open('inputs/06.txt', 'r') as f:
     answer_group = list()
     answer_set = set()
@@ -11,5 +12,23 @@ with open('inputs/06.txt', 'r') as f:
 
     answer_group.append(answer_set)
 
-sum = sum(map(lambda x: len(x), answer_group))
-print(sum)
+sum1 = sum(map(lambda x: len(x), answer_group))
+print("Part 1: " + str(sum1))
+
+# Part 2
+with open('inputs/06.txt', 'r') as f:
+    answer_group = list()
+    answer_set = None
+    for line in f.readlines():
+        if(line == '\n'):
+            answer_group.append(answer_set)
+            answer_set = None
+        else:
+            new_set = set([x for x in line if x != '\n'])
+            answer_set = new_set if answer_set == None else answer_set.intersection(new_set)
+
+
+    answer_group.append(answer_set)
+
+sum2 = sum(map(lambda x: len(x), answer_group))
+print("Part 2: " + str(sum2))
